f24028ab66568105ca183706f7916c9c6a78059b
LeoEsplinio <leoexplinio@gmail.com>
2024-03-22 16:04:15 -0300

    Projeto_Banco_De_Dados

--- Projeto_Banco_de_Dados.sql
+++ Projeto_Banco_de_Dados.sql
@@ -1,1 +1,12 @@
 create database Projeto;
+
+CREATE TABLE Campanha 
+(
+    id_campanha INT PRIMARY KEY,
+    nome VARCHAR(100),
+    data_inicio DATE,
+    data_fim DATE
+);
+
+
+SELECT * FROM  campanha
