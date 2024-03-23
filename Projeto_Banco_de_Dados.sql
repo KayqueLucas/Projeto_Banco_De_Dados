f3d85a9fa4b4aa966dca8dd6e37872793844121c
Matheus Dalbone <matheusdalbone7@gmail.com>
2024-03-23 19:28:37 -0300

    Atualizando tabelas

--- Projeto_Banco_de_Dados.sql
+++ Projeto_Banco_de_Dados.sql
@@ -2,7 +2,7 @@ create database Projeto;
 
 CREATE TABLE Campanha 
 (
-    id_campanha INT PRIMARY KEY,
+    id_campanha serial PRIMARY KEY,
     nome VARCHAR(100),
     data_inicio DATE,
     data_fim DATE
@@ -14,7 +14,7 @@ create table funcionarios
 	departamento varchar(100) not null,
 	nome varchar(100) not null,
 	idade INT check(idade >=18),
-	email varchar(100)
+	email varchar(100) unique not null check(email ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Z|a-z]{2,}$' )
 );
 
 create table perguntas
@@ -25,7 +25,7 @@ create table perguntas
 	opcao_b VARCHAR(255),
 	opcao_c VARCHAR(255),
 	opcao_d VARCHAR(255),
-	resposta_selecionada CHAR(1)
+	resposta_correta CHAR(1)
 );
 
 create table respostas
@@ -33,12 +33,21 @@ create table respostas
 	id_respostas serial primary key,
 	id_funcionarios int,
 	id_perguntas int,
-	resposta_correta char(1),
+	resposta_escolhida char(1),
 	foreign key (id_funcionarios) references funcionarios(id_funcionarios),
 	foreign key (id_perguntas) references perguntas(id_pergunta)
 );
 
+create table premiacao 
+(
+	id_premiacao serial primary key,
+	num_respostas_corretas int,
+	id_funcionarios int,
+	foreign key (id_funcionarios) references funcionarios(id_funcionarios)
+);
+
 select * from perguntas;
 select * from funcionarios;
-SELECT * FROM  campanha;
+SELECT * FROM campanha;
 select * from respostas;
+select * from premiacao;
